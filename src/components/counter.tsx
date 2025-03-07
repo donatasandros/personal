6a5d6a5e859728e955a598efcfe8c1d04418bc07
donatas <donatas.andros@gmail.com>
2025-03-07 21:07:42 +0200

    fix lint error

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 
 export function Counter() {
-  const [count, setCount] = React.useState(0);
+  const [count] = React.useState(0);
 
   return (
     <div className="inline-block bg-white border-2 border-[#aca899] px-2 py-1 text-sm">
