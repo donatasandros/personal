69f91296014e2276898d6ba89a8cba2747a0e7cf
donatas <donatas.andros@gmail.com>
2025-03-08 22:07:40 +0200

    update web metadata, add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,48 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { FakeVirusAlert } from "@/components/misc/fake-virus-alert";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { SITE_CONFIG } from "@/config/site";
 
 export const metadata: Metadata = {
-  title: "Donatas Andros",
-  description:
-    "Junior frontend developer passionate about creating clean, user-friendly web experiences. Eager to learn and grow, I enjoy exploring new technologies and building interactive projects that improve user experience.",
+  title: SITE_CONFIG.name,
+  metadataBase: new URL(SITE_CONFIG.url),
+  description: SITE_CONFIG.description,
+  keywords: [
+    "frontend",
+    "developer",
+    "Vilnius",
+    "Lithuania",
+    "nextjs",
+    "react",
+    "sveltekit",
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: SITE_CONFIG.url,
+    title: SITE_CONFIG.name,
+    description: SITE_CONFIG.description,
+    images: [
+      {
+        url: SITE_CONFIG.ogImage,
+        width: 1200,
+        height: 630,
+        alt: SITE_CONFIG.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: SITE_CONFIG.name,
+    description: SITE_CONFIG.description,
+    images: [SITE_CONFIG.ogImage],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-32x32.jpg",
+    apple: "/apple-touch-icon.jpg",
+  },
+  manifest: `${SITE_CONFIG.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
---
+++ src/config/site.ts
@@ -0,0 +1,7 @@
+export const SITE_CONFIG = {
+  name: "Donatas Andros",
+  url: "https://www.andros.lt",
+  ogImage: "https://www.andros.lt/og.jpg",
+  description:
+    "Junior frontend developer currently based in Vilnius, Lithuania. Get in touch today!",
+};
